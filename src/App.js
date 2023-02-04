c53be84d78bc95890c99cd43b46c0415ce756ae7
colins1 <maxrozhkov1@gmail.com>
2023-02-04 20:07:02 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './main.css';
-import Listing from './componets/Listing';
+import Listing from './components/Listing';
 
 function App() {
   return (
--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,6 +1,6 @@
-function Listing () {
-
-
+function Listing (props) {
+    const {items} =  props
+    return (<div>{items}</div>);
 }
 
 export default Listing
