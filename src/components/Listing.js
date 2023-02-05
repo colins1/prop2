8414c5e362ff90a5afe87424638182d8f52f409e
colins1 <maxrozhkov1@gmail.com>
2023-02-05 17:05:15 +0300

    add

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,30 +3,21 @@ import React from 'react';
 function Listing ({items}) {
     return (
         <div className="item-list">
-            {items.data.map( (itm) => <div className="item">
-                <div className="item-image">
-                <a href="https://www.etsy.com/listing/292754135/woodland-fairy">
+            {items.data.map( (itm) => itm.MainImage != undefined ? <div className="item">
+                <div key={itm.listing_id} className="item-image">
+                <a href={itm.url}>
                     <img src={itm.MainImage.url_570xN}/>
                 </a>
                 </div>
                 <div className="item-details">
                 <p className="item-title">{itm.title}</p>
-                <p className="item-price">$3.99</p>
-                <p className="item-quantity level-medium">12 left</p>
+                <p className="item-price">{itm.currency_code == 'USD' ?  `$${itm.price}` : itm.currency_code == 'EUR' ? `€${itm.price}` : `${itm.price} ${itm.currency_code}`}</p>
+                <p className={itm.quantity > 20 ? "item-quantity level-high" : itm.quantity > 10 ? "item-quantity level-medium" : "item-quantity level-low"}>{itm.quantity} left</p>
                 </div>
-            </div>)}
+            </div> : "")}
         </div>
 
     );
 }
-
-/*listing_id — уникальный идентификатор предложения, число;
-url — ссылка на предложение, строка;
-MainImage — информация об изображении, объект, нам необходимо использовать свойство url_570xN для получения адреса главной картинки, строка;
-title — название предложения, строка;
-currency_code — код валюты, строка;
-price — цена, строка;
-quantity — доступное количество, число.
-*/
 
 export default Listing
